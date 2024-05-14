0b99ce479a7d5bac12083a5e77550db843563a90
Alerechi Ordu <alerechiordu@gmail.com>
2024-05-14 02:34:48 +0100

    intro to JS first commit

--- variables.js
+++ variables.js
@@ -8,10 +8,30 @@
 // // declare variable num2 and assign 6 to it
 // let num2 = 6;
 
-// assign 5 to variable score
-let score = 5; 
-console.log(score); // 5
 
-// change the value of score to 3
-score = 3; 
-console.log(score); // 3
+// Program to check if the number is positive
+
+// const number = prompt("Enter a number: ");
+
+// // check if number is greater than 0
+// if (number > 0) {
+//     // the body of the if statement
+//     console.log("positive number");
+// }
+
+// console.log("nice number");
+
+// // JavaScript if…else Statement
+// let age = 17;
+
+// // if age is 18 or above, you are an adult
+// // otherwise, you are a minor
+
+// if (age >= 18) {
+//     console.log("You are an adult");
+// }
+// else {
+//     console.log("You are a minor");
+// }
+
+// // Output: You are a minor
