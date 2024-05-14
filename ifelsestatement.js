5f77f1ba05273ef12d4d66abd19f33360cd12c8d
Alerechi Ordu <alerechiordu@gmail.com>
2024-05-14 02:35:56 +0100

    intro to JS second commit

---
+++ ifelsestatement.js
@@ -0,0 +1,39 @@
+// JavaScript if...else Statement
+// The JavaScript if...else statement is used to execute/skip a block of code based on a condition.
+
+// Here's a quick example of the if...else statement. You can read the rest of the tutorial if you want to learn about if...else in greater detail.
+
+// // Example
+// let score = 45;
+
+// // check if score is fifty or greater
+// if (score >= 50) {
+//     console.log("You passed the examination.");
+// }
+// else {
+//     console.log("You failed the examination.");
+// }
+
+// // Output: You failed the examination.
+
+// let rechi = "King"
+
+// if (rechi == "King1"){
+//     console.log("You are the king")
+// }
+// else{
+//     console.log("you are not the king")
+// }
+
+
+// Program to check if the number is positive
+
+const number = prompt("Enter a number: ");
+
+// check if number is greater than 0
+if (number > 0) {
+    // the body of the if statement
+    console.log("positive number");
+}
+
+console.log("nice number");
